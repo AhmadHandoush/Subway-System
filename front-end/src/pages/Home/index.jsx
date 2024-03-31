31971610c2be178b803e610b2b1b11cba8718e81
hamada <hamadahandoush9991@gmail.com>
2024-03-31 17:32:25 +0200

    finish hero section

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
+import Hero from "./components/Hero";
 import "./home.css";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <Hero />
+      </div>
+    </div>
+  );
 }
 
 export default Home;

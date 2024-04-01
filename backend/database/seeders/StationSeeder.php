55768025f5e7fe75ae822a84419a68af29ab6b65
kamilawad1 <kamilawad15@gmail.com>
2024-04-01 16:43:06 +0300

    added station seeders

---
+++ backend/database/seeders/StationSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Station;
+
+class StationSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        Station::create([
+            'name' => 'Beirut Station',
+            'open_hour' => '06:00:00',
+            'close_hour' => '22:00:00',
+            'latitude' => 33.8886,
+            'longitude' => 35.4955,
+            'status' => 'Active',
+            'image' => 'station1.jpg',
+            'manager_id' => 5,
+        ]);
+
+        Station::create([
+            'name' => 'Akkar Station',
+            'open_hour' => '06:00:00',
+            'close_hour' => '22:00:00',
+            'latitude' => 34.5329,
+            'longitude' => 36.1328,
+            'status' => 'Active',
+            'image' => 'station2.jpg',
+            'manager_id' => 6,
+        ]);
+    }
+}

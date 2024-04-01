24250cc934df7e2b2d31cdf7a531516a2b7bd71d
kamilawad1 <kamilawad15@gmail.com>
2024-04-01 19:36:03 +0300

    added get stations api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StationController;
 
-Route::get('/user', function (Request $request) {
+
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+Route::get('get_stations', [StationController::class, "get_all_stations"]);

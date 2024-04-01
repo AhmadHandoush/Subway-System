ea9f034aec53b5e61346d966f53bc75032803259
hamada <hamadahandoush9991@gmail.com>
2024-04-01 03:34:19 +0200

    complete ui home page

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Popular from "./components/Popular";
 import Title from "./components/Title";
 import "./home.css";
 
@@ -7,7 +8,10 @@ function Home() {
     <div className="home">
       <div className="container">
         <Hero />
-        <Title>Popular Stations.</Title>
+        <Title>Popular Stations</Title>
+        <Popular />
+        <Title>Recommended Stations</Title>
+        <Popular />
       </div>
     </div>
   );
